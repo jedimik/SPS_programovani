317d7bf2c9a9c6de0036a582544c9e3506b82b48
Jedimik <krajca.tomascz@gmail.com>
2020-12-08 16:03:26 +0100

    hodina

---
+++ EP2/EP11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EP11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listik = new List<int>();
+            int a;
+
+            do
+            {
+                Console.WriteLine("Zadej hodnotu - cele cislo, proste integer");
+                a = Int32.Parse(Console.ReadLine());
+                if (a == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    listik.Add(a);
+                }
+            }
+            while (a != 0);
+
+            for (int i = 0; i < listik.Count; i++)
+            {
+                Console.WriteLine("V listíku máme hodnotz:{0}", listik[i]);
+            }
+
+
+        }
+    }
+}
