46a1bb43ae734779c37e9052a29d0bc37a17ae62
Jedimik <krajca.tomascz@gmail.com>
2020-12-08 09:40:50 +0100

    8.12 navigacni menu vlevo, grid

--- IT2/ITProjekt/index.php
+++ IT2/ITProjekt/index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF8">
 <title> Hodina 07 CSS a HTML </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="Styles/style.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet">
@@ -11,23 +12,28 @@
 <?php
 include 'Styles/header.html';
 ?>
-<!--Hypertexty-->
-<!--
-<a href="https://www.seznam.cz"> Odkaz na seznam </a> <br>
-<a href="https://www.seznam.cz"> <img style="width:30%;" src="Files/OW.png" alt="ow"> </a>  <br>
-<a href="https://www.seznam.cz"> <button type="button">Hypertext </button> </a>
--->
 
-<a href="SubPages/ukazme.php"> Odkaz na seznam </a> <br>
-<!--
-<a href="ukazme.html"> <img style="width:30%;" src="Files/OW.png" alt="ow"> </a>  <br>
-<a href="ukazme.html"> <button type="button">Hypertext </button> </a>
--->
+<div class="grid">
+    <p class="nadpis"> Vítejte na našem nově vytvořeném bazaru!!! </p>
+    <p class="text1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+         Pellentesque pretium lectus id turpis. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+         Nullam at arcu a est sollicitudin euismod. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.
+          Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus.
+           Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Etiam sapien elit, consequat eget,
+            tristique non, venenatis quis, ante. Curabitur vitae diam non enim vestibulum interdum.
+             Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+              nisi ut aliquid ex ea commodi consequatur? <span style="white-space: pre-line"> <!-- \n -->           
+             </span> Vivamus porttitor turpis ac leo. In laoreet,
+               magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero.
+                Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. In rutrum. Etiam quis quam.
+                 Aliquam erat volutpat. Etiam neque. Ut enim ad minima veniam,
+                  quis nostrum exercitationem ullam corporis suscipit laboriosam,
+                   nisi ut aliquid ex ea commodi consequatur? Curabitur sagittis hendrerit ante.
+                    Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+                     nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 
-<!--
-<div class="header">
-<h1> Můj bazárek :] <h1>
-</div> -->
+Maecenas aliquet accumsan leo. Nullam faucibus mi quis velit. Etiam egestas wisi a erat. Praesent vitae arcu tempor neque lacinia pretium. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Ut tempus purus at lorem. Aenean id metus id velit ullamcorper pulvinar. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Fusce nibh. Nullam dapibus fermentum ipsum. Integer malesuada. </p>
+</div>
 
 
 <?php
