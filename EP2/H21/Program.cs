32a03b98e42a5fc901125926560ec52a62154695
Jedimik <krajca.tomascz@gmail.com>
2021-05-07 10:50:07 +0200

    dopln vyuka

---
+++ EP2/H21/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace H21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jmeno = "Tomas";
+            string prijmeni = "Krajca";
+            string jmenoprijmeni = jmeno +" "+ prijmeni;
+            //Spojovat / Contatenate
+            string jmenoprijmeni2 = string.Concat(string.Concat(jmeno," "), prijmeni);
+            jmenoprijmeni2 = jmenoprijmeni2.Remove(3);
+            //Console.WriteLine(jmenoprijmeni2);
+            string workingDir = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine(workingDir);
+            // Odstranit vse po H21, 
+            Console.WriteLine(workingDir.IndexOf("H21")); // vraci 28
+            string control = "H21";
+            workingDir=workingDir.Remove(workingDir.IndexOf(control) + control.Length + 1);
+            /*
+            for (int i = 0; i < workingDir.Length-2; i++)
+            {
+                if (workingDir[i] == 'H' && workingDir[i+1]=='2' && workingDir[i+2]=='1')
+                {
+                    Console.WriteLine("zde");
+                    workingDir=workingDir.Remove(i + 4);
+                    break;
+                }
+                //Console.WriteLine(workingDir[i]);
+            }*/
+            Console.WriteLine(workingDir);
+            /*foreach (char o in workingDir)
+            {
+                Console.WriteLine(o);
+            }*/
+            // Nacteni cesty soubor.txt
+            workingDir += @"Soubory\soubor.txt";
+            //workingDir += "Soubory\\soubor.txt";
+            Console.WriteLine(workingDir);
+            string workingDirAbsolute = @"C:\Users\krajc\source\repos\H21\Soubory\soubor.txt";
+            string workindDirRelative = "../../../Soubory/soubor.txt";
+            Console.WriteLine("Relativni cesta:{0}", workindDirRelative);
+            Console.WriteLine("Absolutni cesta:{0}", workingDirAbsolute);
+
+            using (StreamReader sr = new StreamReader(workindDirRelative))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null) //EOF E0F
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            // Druhy zpusob
+            string soubor = System.IO.File.ReadAllText(workingDirAbsolute);
+            Console.WriteLine("\n\nObsah souboru:{0}", soubor);
+            // Treti zpusob
+            string[] souborek = System.IO.File.ReadAllLines(workingDirAbsolute);
+            Console.WriteLine("\n\nObsah pole:");
+            foreach (string line in souborek)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EP2/H22/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace H22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cesta = @"C:\Users\krajc\source\repos\H22\ProCteni\soubor.txt";
+            string radek;
+            using (StreamReader sr = new StreamReader(cesta))
+            {
+                while ((radek = sr.ReadLine()) != null)
+                {
+                    if (radek.Contains("mys"))
+                    {
+                        Console.WriteLine(radek);
+                    }
+                }
+
+            }
+            /*
+            string cesta = @"C:\Users\krajc\source\repos\H22\ProCteni\soubor.txt";
+            string radek;
+            List<string> obsahsouboru = new List<string>();
+            string druhaMoznostObsahu = "";
+            using (StreamReader sr = new StreamReader(cesta))
+            {                
+                while ((radek = sr.ReadLine()) != null)
+                {
+                    if (radek.Contains("ipsum"))
+                    {
+                        // int index = radek.IndexOf("ipsum");
+                        // radek = radek.Substring(index, index+4) + "ipsum".ToUpper() + radek.Substring(index + 5);
+                        
+                        radek = Regex.Replace(radek, "ipsum", "IPSUM");
+                        druhaMoznostObsahu += radek + "\n";
+                        obsahsouboru.Add(radek);
+                        Console.WriteLine(radek);
+                    }
+                }
+
+            }*/
+            /*
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < obsahsouboru.Count; i++)
+            {
+                Console.WriteLine(obsahsouboru[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(druhaMoznostObsahu);
+            */
+            /*
+            string cesta = @"C:\Users\krajc\source\repos\H22\ProCteni\Konverzace.txt";
+            string radek;
+            using (StreamReader sr = new StreamReader(cesta))
+            {
+                while ((radek = sr.ReadLine()) != null)
+                {
+                    //Kdyz pise on
+                    if (radek.Contains("Ja:"))
+                    {
+                        Console.WriteLine("Ja jsem ji laskyplne napsal:" + radek.Replace("Ja:", ""));
+                    }
+                    // nebo Kdyz pise ona
+                    else if (radek.Contains("Ona:"))
+                    {
+                        Console.WriteLine("Ona z povysenosti odpovedela:" + radek.Replace("Ona:", ""));
+                    }
+                    // nebo kdyz pise facebook
+                    else if (radek.Contains("Facebook:"))
+                    {
+                        Console.WriteLine("Byl jste zablokovan.");
+                        Console.WriteLine(radek.Replace("Facebook:", "").Trim());
+                    }
+                }               
+            }
+            */
+            /*
+            string str = "....Ahoj. jak se mas!!!§§§";
+            char[] charyOdstran = { '§', '.' };
+            string cistystr = str.Trim(charyOdstran);
+            string jinakcistystring = str.Replace(".", "");            
+            Console.WriteLine(cistystr);
+            Console.WriteLine(jinakcistystring);
+            */
+            string[] strDel = { "se", "jak" };
+            string str = "Ahoj jak se mas!";
+            for (int i = 0; i < strDel.Length; i++)
+            {
+                str=str.Replace(strDel[i], "");
+            }
+            Console.WriteLine(str);
+
+        }
+    }
+}
